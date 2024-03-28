0d1cc9a3b5fd81ec935dad6af157ae63ef6a6141
Maarten Sebregts <msebregts@ignitioncomputing.com>
2024-03-28 15:46:22 +0100

    Add tests for generic rule set: throw random data (but consistent in coordinate shapes) into it and expect the validators don't crash

---
+++ tests/integration/test_generic_ruleset.py
@@ -0,0 +1,23 @@
+import imaspy
+import pytest
+from imaspy.test.test_helpers import fill_consistent
+
+from ids_validator.validate.validate import validate
+
+
+@pytest.mark.parametrize("ids_name", imaspy.IDSFactory().ids_names())
+def test_generic_tests_with_randomly_generated_ids(ids_name, tmp_path):
+    if ids_name == "amns_data":
+        pytest.skip("amns_data IDS is not supported by IMASPy's fill_consistent")
+
+    ids = imaspy.IDSFactory().new(ids_name)
+    fill_consistent(ids)
+
+    uri = f"imas:ascii?path={tmp_path}"
+    dbentry = imaspy.DBEntry(uri, "w")
+    dbentry.put(ids)
+    dbentry.close()
+
+    results = validate(uri)
+    for result in results:
+        assert result.exc is None  # Generic tests should not lead to an Exception
