9d7e9790e6a9ec67e755d1d719fcba5f906493d6
wasikj <jwasik@man.poznan.pl>
2024-04-03 13:43:42 +0200

    delete garbage

--- ids_validator/cli/set-iter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
- 
-export PYTHONPATH=$PYTHONPAT:`pwd`/../../
-export RULESET_PATH=`pwd`/../../tests/rulesets/env_var
-
-. ../../venv/bin/activate
-
-module load IMAS/3.40.1-5.1.0-foss-2020b
-module load IMASPy/1.0.0-foss-2020b
