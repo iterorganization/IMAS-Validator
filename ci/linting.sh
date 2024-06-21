78c6b6bcc4f4ff842529728bdc82d5b93136119d
Maarten Sebregts <msebregts@ignitioncomputing.com>
2024-06-21 09:04:53 +0200

    Allow specifying modules to load in the CI configuration

--- ci/linting.sh
+++ ci/linting.sh
@@ -10,8 +10,8 @@ echo "Loading modules..."
 . /usr/share/Modules/init/sh
 module purge
 # Load modules required for linting
-# - Python (obviously)
-module load Python/3.8.6-GCCcore-10.2.0
+# Modules are supplied as arguments in the CI job:
+module load $@
 
 # Debuggging:
 echo "Done loading modules"
