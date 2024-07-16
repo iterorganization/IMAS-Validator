78b7daac01890460edf86a67464c20d107cf3819
Simon Pinches <Simon.Pinches@iter.org>
2024-07-16 12:19:48 +0200

    Summary IDS: Check that q >=0 for ITER

--- ids_validator/assets/rulesets/iter_scenarios/summary.py
+++ ids_validator/assets/rulesets/iter_scenarios/summary.py
@@ -28,23 +28,27 @@ def validate_mandatory_values(ids):
     assert ids.global_quantities.tau_energy.value.has_value
     assert ids.global_quantities.v_loop.value.has_value
     assert ids.global_quantities.q_95.value.has_value
-
-    # local
     assert ids.local.separatrix.n_e.value.has_value
-    # local.separatrix.n_i.*.value
     assert ids.local.separatrix.n_i.has_value
     assert ids.local.separatrix.zeff.value.has_value
 
 
 @validator("summary")
 def validate_global_quantities_b0(ids):
-    """Validate that global_quantities/b0/value(:) is b0 < 0."""
+    """Validate that global_quantities/b0/value(:) is -8 T <= b0 <= 0."""
 
-    assert ids.global_quantities.b0.value < 0.0
+    assert -8 <= ids.global_quantities.b0.value <= 0.0
 
 
 @validator("summary")
 def validate_global_quantities_ip(ids):
     """Validate that global_quantities/ip/value(:) is -17000000 < ip <= 0."""
 
-    assert -17000000 < ids.global_quantities.ip.value <= 0.0
+    assert -17000000 <= ids.global_quantities.ip.value <= 0.0
+
+
+ @validator("summary")
+def validate_global_quantities_q_95(ids):
+    """Validate that global_quantities/q_95/value(:) is q_95 >= 0"""
+
+    assert ids.global_quantities.q_95.value >= 0.0
