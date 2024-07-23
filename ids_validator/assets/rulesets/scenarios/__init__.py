c569c9945568e003848cef52d100ce225a383e9a
Olivier Hoenen <Olivier.Hoenen@iter.org>
2024-07-23 11:40:37 +0200

    More code style edit

--- ids_validator/assets/rulesets/scenarios/__init__.py
+++ ids_validator/assets/rulesets/scenarios/__init__.py
@@ -1,6 +1,7 @@
 """Ruleset with data validation rules specific to the ITER tokamak.
 
-These rules test that some key physics values are provided. Please refer to the web page[1].
+These rules test that some key physics values are provided.
+Please refer to the web page[1].
 
-[1] https://confluence.iter.org/display/IMP/Required+fields+in+a+dataset+to+be+imported+in+a+scenario+database
+[1] https://confluence.iter.org/display/IMP/Required+fields+in+a+dataset+to+be+imported+in+a+scenario+database # noqa: E501
 """
