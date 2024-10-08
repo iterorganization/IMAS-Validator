4c6086e3a9dda0673bf032be710c827aa6d029e3
Mike Sanders <msanders@ignitioncomputing.com>
2024-10-08 10:47:04 +0200

    optimized increasing time by doing inclusion via metadata name instead of full aos

--- ids_validator/assets/rulesets/generic/generic.py
+++ ids_validator/assets/rulesets/generic/generic.py
@@ -33,7 +33,7 @@ def validate_homogeneous_time(ids):
 @validator("*")
 def validate_increasing_time(ids):
     """Validate that all non-empty time vectors are strictly increasing."""
-    dynamic_aos = []
+    aos_dict = {}
     for time_quantity in Select(ids, "(^|/)time$", has_value=True):
         # 1D time array:
         if time_quantity.metadata.ndim == 1:
@@ -43,11 +43,12 @@ def validate_increasing_time(ids):
             # Get the corresponding AoS quantity (e.g. profiles_1d for
             # profiles_1d[0].time):d
             aos = Parent(time_quantity, 2)
-            if aos not in dynamic_aos:
-                dynamic_aos.append(aos)
+            aos_name = aos.metadata.name
+            if aos_name in aos_dict.keys():
+                aos_dict[aos_name] = aos
 
     # Validate time "vectors" for timed arrays of structures
-    for aos in dynamic_aos:
+    for aos in aos_dict.values():
         last_time = float("-inf")
         for struct in aos:
             assert (
