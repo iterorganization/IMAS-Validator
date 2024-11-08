377407cb81334d16544211ae3ff1d016c4024266
Sebbe <sblokhuizen@ignitioncomputing.com>
2024-11-08 16:41:50 +0100

    add more ggd rules

--- ids_validator/assets/rulesets/generic/ggd.py
+++ ids_validator/assets/rulesets/generic/ggd.py
@@ -1,6 +1,8 @@
 """Rules applying to all IDSs containing GGDs"""
+
 from imaspy.ids_defs import IDS_TIME_MODE_HOMOGENEOUS
 from imaspy import identifiers
+import imaspy
 
 SUPPORTED_IDS_NAMES = (
     "edge_profiles",
@@ -12,14 +14,26 @@ SUPPORTED_IDS_NAMES = (
     "wall",
 )
 
-EXPERIMENTAL_IDS_NAMES = [
-    "equilibrium",
-    "distribution_sources",
-    "distributions",
-    "tf",
-    "transport_solver_numerics",
-    "waves",
-]
+#   EXPERIMENTAL_IDS_NAMES = [
+#       "equilibrium",
+#       "distribution_sources",
+#       "distributions",
+#       "tf",
+#       "transport_solver_numerics",
+#       "waves",
+#   ]
+
+def get_ggd(ids):
+    """Get all GGD base nodes"""
+    ggd_list = []
+    for node in Select(ids,"(^|/)ggd$", leaf_only=False):
+        if node.metadata.name == "ggd":
+            parent_node = imaspy.util.get_parent(node._obj)
+            if parent_node.metadata.name != "ggd":
+                ggd_list.append(node)
+    return ggd_list
+
+
 
 def validate_identifier(identifier):
     """Validate that an identifier has its name, index and description filled"""
@@ -27,23 +41,28 @@ def validate_identifier(identifier):
     assert identifier.index.has_value
     assert identifier.description.has_value
 
+
 def is_index_in_identifiers(index, identifier_list):
     """Checks if an index appears in an identifier list"""
     return any(index == member.value for member in identifier_list)
 
+
 # TODO: only test on GGD IDS instead of all
 
-# Grid rules 
+
+# Grid rules
 @validator("*")
 def validate_grid_ggd_identifier(ids):
     """Validate that the identifiers of all grid_ggds are filled"""
     for grid_ggd in ids.grid_ggd:
         validate_identifier(grid_ggd.identifier)
 
+
 @validator("*")
 def validate_grid_ggd_length(ids):
     """Validate that there are as many grids as there are time steps"""
     assert len(ids.grid_ggd) == len(ids.time)
+
 
 @validator("*")
 def validate_grid_ggd_time_homogeneous(ids):
@@ -51,6 +70,7 @@ def validate_grid_ggd_time_homogeneous(ids):
     if ids.ids_properties.homogeneous_time == IDS_TIME_MODE_HOMOGENEOUS:
         for grid_ggd in ids.grid_ggd:
             assert not grid_ggd.time.has_value
+
 
 # Space rules
 @validator("*")
@@ -60,13 +80,17 @@ def validate_space_identifier(ids):
         for space in grid_ggd.space:
             validate_identifier(space.identifier)
 
+
 @validator("*")
 def validate_space_coordinates_type_identifier(ids):
     """Validate that the coordinate type identifiers match"""
     for grid_ggd in ids.grid_ggd:
         for space in grid_ggd.space:
             for coord_type in space.coordinates_type:
-                assert is_index_in_identifiers(coord_type, identifiers.coordinate_identifier)
+                assert is_index_in_identifiers(
+                    coord_type, identifiers.coordinate_identifier
+                )
+
 
 @validator("*")
 def validate_space_geometry_type_identifier(ids):
@@ -75,6 +99,7 @@ def validate_space_geometry_type_identifier(ids):
     for grid_ggd in ids.grid_ggd:
         for space in grid_ggd.space:
             assert space.geometry_type.index >= 0
+
 
 # Objects_per_dimension rules
 @validator("*")
@@ -92,11 +117,14 @@ def validate_obj_per_dim_geometry_content(ids):
                     elif dim == 2:
                         assert geometry_content in {31, 32}
                     else:
-                        assert False, "Geometry content undefined for n-dimensional objects with n>2"
+                        assert (
+                            False
+                        ), "Geometry content undefined for n-dimensional objects with n>2"
+
 
 @validator("*")
 def validate_obj_per_dim_geometry_length(ids):
-    """Validate that the geometry of the objects have the correct length, according to 
+    """Validate that the geometry of the objects have the correct length, according to
     its geometry_content"""
     for grid_ggd in ids.grid_ggd:
         for space in grid_ggd.space:
@@ -116,4 +144,144 @@ def validate_obj_per_dim_geometry_length(ids):
                         elif geometry_content == 32:
                             assert len(geometry) == 5
                     else:
-                        assert len(geometry) == len(space.coordinates_type)
+                        if geometry.has_value:
+                            assert len(geometry) == len(space.coordinates_type)
+
+
+@validator("*")
+def validate_obj_0D_geometry_length(ids):
+    """Validate that the geometry of 0D objects is larger than zero."""
+    for grid_ggd in ids.grid_ggd:
+        for space in grid_ggd.space:
+            obj_0D = space.objects_per_dimension[0]
+            for obj in obj_0D.object:
+                assert len(obj.geometry) > 0
+
+
+@validator("*")
+def validate_obj_per_dim_nodes_length(ids):
+    """Validate that the nodes of the objects have the correct length. 
+    0D objects should be empty or contain themselves, edges should contain 2 
+    nodes, while n-order should contain at least n+1 nodes."""
+    for grid_ggd in ids.grid_ggd:
+        for space in grid_ggd.space:
+            for dim, obj_per_dim in enumerate(space.objects_per_dimension):
+                for i, obj in enumerate(obj_per_dim.object):
+                    nodes = obj.nodes
+                    if dim == 0:
+                        if nodes.has_value:
+                            assert nodes == [i + 1]
+                    elif dim == 1:
+                        assert len(nodes) == 2
+                    else:
+                        assert len(nodes) >= dim + 1
+
+@validator("*")
+def validate_obj_per_dim_nodes(ids):
+    """Validate that the filled object nodes point to existing nodes in the grid."""
+    for grid_ggd in ids.grid_ggd:
+        for space in grid_ggd.space:
+            len_0D_obj = len(space.objects_per_dimension[0].object)
+            for obj_per_dim in space.objects_per_dimension:
+                for obj in obj_per_dim.object:
+                    if obj.nodes.has_value:
+                        for node in obj.nodes:
+                            assert node <= len_0D_obj
+
+@validator("*")
+def validate_obj_per_dim_measure_empty(ids):
+    """Validate that the measure value of 0D objects is empty."""
+    for grid_ggd in ids.grid_ggd:
+        for space in grid_ggd.space:
+            obj_0D = space.objects_per_dimension[0]
+            for obj in obj_0D.object:
+                assert not obj.measure.has_value
+
+
+# Grid subset rules
+@validator("*")
+def validate_grid_subset_identifier(ids):
+    """Validate the grid subset identifier."""
+    for grid_ggd in ids.grid_ggd:
+        for grid_subset in grid_ggd.grid_subset:
+            validate_identifier(grid_subset.identifier)
+
+
+@validator("*")
+def validate_grid_subset_length(ids):
+    """Validate that the grid has at least 1 grid subset."""
+    for grid_ggd in ids.grid_ggd:
+        assert len(grid_ggd.grid_subset) > 0
+
+
+def is_index_in_aos_identifier(aos, index):
+    """Check if an index appears exactly once in the identifier of an AoS."""
+    matches = sum(
+        1 for structure in aos if structure.identifier.index == index
+    )
+    assert matches == 1
+
+def find_index_match_in_aos_identifier(aos, index):
+    """Return the first object in 'aos' whose identifier.index matches the given index, or None if no match is found."""
+    for structure in aos:
+        if structure.identifier.index == index:
+            return structure
+    return None 
+
+@validator("*")
+def validate_grid_subset_space_index(ids):
+    """Validate that the space in the subset points to an existing space in the grid."""
+    for grid_ggd in ids.grid_ggd:
+        for grid_subset in grid_ggd.grid_subset:
+            for element in grid_subset.element:
+                for object in element.object:
+                    space_idx = object.space
+                    is_index_in_aos_identifier(grid_ggd.space, space_idx)
+
+@validator("*")
+def validate_grid_subset_dimension_index(ids):
+    """Validate that the dimension in the subset points to an existing dimension in the grid."""
+    for grid_ggd in ids.grid_ggd:
+        for grid_subset in grid_ggd.grid_subset:
+            for element in grid_subset.element:
+                for object in element.object:
+                    dim = object.dimension
+                    space_idx = object.space
+                    space = find_index_match_in_aos_identifier(grid_ggd.space, space_idx)
+                    assert len(space.objects_per_dimension) >= dim
+
+
+@validator("*")
+def validate_grid_subset_object_index(ids):
+    """Validate that the object index in the subset points to an existing object in the grid."""
+    for grid_ggd in ids.grid_ggd:
+        for grid_subset in grid_ggd.grid_subset:
+            for element in grid_subset.element:
+                for object in element.object:
+                    dim = object.dimension
+                    space_idx = object.space
+                    obj_idx = object.index
+                    space = find_index_match_in_aos_identifier(grid_ggd.space, space_idx)
+                    assert (
+                        len(space.objects_per_dimension[dim._obj - 1].object) >= obj_idx
+                    )
+
+
+@validator("*")
+def validate_grid_subset_obj_dimension(ids):
+    """Validate that the dimensions of the object are not larger than the object of the grid subset."""
+    for grid_ggd in ids.grid_ggd:
+        for grid_subset in grid_ggd.grid_subset:
+            subset_dim = grid_subset.dimension
+            for element in grid_subset.element:
+                for object in element.object:
+                    obj_dim = object.dimension
+                    assert subset_dim >= obj_dim
+
+# GGD rules                    
+@validator("*")
+def validate_ggd_length(ids):
+    """Validate that the dimensions of the GGD match the number of time steps."""
+    ggd_list = get_ggd(ids)
+    for ggd in ggd_list:
+        assert len(ggd) == len(ids.time)
