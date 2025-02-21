8d35f6bdd3d97e1a4269cf0dbd9cfd0dcc775e65
Sebbe <sblokhuizen@ignitioncomputing.com>
2025-02-21 14:42:18 +0100

    fix mypy error

--- ids_validator/validate/ids_wrapper.py
+++ ids_validator/validate/ids_wrapper.py
@@ -3,7 +3,7 @@ This file describes the overload class for the operators
 """
 
 import operator
-from typing import Any, Callable, Collection, List, Optional, Tuple
+from typing import Any, Callable, Collection, Iterator, List, Optional, Tuple
 
 import numpy as np
 from imaspy.ids_primitive import IDSPrimitive
@@ -115,7 +115,7 @@ class IDSWrapper:
     def __repr__(self) -> str:
         return f"IDSWrapper({self._obj!r})"
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[int]:
         return iter(self._obj)
 
     def __str__(self) -> str:
