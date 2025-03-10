d76eb6a84a98ceb6acbdc703ba70e28b183d6bb9
sawantp1 <prasadparshuram.sawantdesai@iter.org>
2025-03-10 15:30:31 +0100

    blackify __init__.py, added install command in asv configuration

--- ids_validator/__init__.py
+++ ids_validator/__init__.py
@@ -10,5 +10,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 connect_formatter(logger)
 
+
 def get_project_root() -> Path:
     return Path(__file__).resolve().parent.parent
