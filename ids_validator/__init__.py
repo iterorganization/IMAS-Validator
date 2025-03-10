a681f3d8ceab4ee69d4f1da0a974c6a02313b09e
sawantp1 <prasadparshuram.sawantdesai@iter.org>
2025-03-10 15:41:46 +0100

    asv install with wheel_dir

--- ids_validator/__init__.py
+++ ids_validator/__init__.py
@@ -6,6 +6,7 @@ from ids_validator.setup_logging import connect_formatter
 from ._version import version  # noqa: F401
 from ._version import version_tuple  # noqa: F401
 
+__version__ = version
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 connect_formatter(logger)
