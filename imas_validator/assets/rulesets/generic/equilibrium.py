163722145e35804acf911d564711376b55f51702
MasanariHosokawa <masanari.hosokawa@iter.org>
2025-07-02 09:17:04 +0200

    changes for review's comments

--- imas_validator/assets/rulesets/generic/equilibrium.py
+++ imas_validator/assets/rulesets/generic/equilibrium.py
@@ -3,97 +3,143 @@
 import numpy as np
 
 
+def ip_b0_sign(ids):
+    """
+    Get the sign of Ip and B0 for each time slice in the IDS.
+    """
+
+    # Initialize lists to store the signs of Ip and B0
+    ipsigns = []
+    b0signs = []
+
+    for itime, time_slice in enumerate(ids.time_slice):
+        # Get the time for this time slice
+        if ids.ids_properties.homogeneous_time == 0:
+            time = time_slice.time
+        else:
+            time = ids.time[itime]
+
+        # Set sign of Ip
+        if time_slice.global_quantities.ip.has_value:
+            ipsign = np.sign(time_slice.global_quantities.ip)
+        else:
+            ipsign = -9
+
+        # Set sign of B0
+        if ids.vacuum_toroidal_field.b0.has_value:
+            b0 = np.interp(time, ids.time, ids.vacuum_toroidal_field.b0)
+            b0sign = np.sign(b0)
+        else:
+            b0sign = -9
+
+        # Append the signs to the lists
+        ipsigns.append(ipsign)
+        b0signs.append(b0sign)
+
+    return ipsigns, b0signs
+
+
+@validator("equilibrium")
+def validate_sign_common(ids):
+    """
+    Validate that the equilibrium IDS has consistent sign conventions.
+    """
+
+    # Get the sign of Ip and B0 for each time slice
+    ipsigns, b0signs = ip_b0_sign(ids)
+
+    # Loop through each time slice
+    for itime, time_slice in enumerate(ids.time_slice):
+
+        # Set the sign of Ip and B0 for this time slice
+        ipsign = ipsigns[itime]
+        b0sign = b0signs[itime]
+
+        #
+        # Validate the sign conventions for EM fields
+        #
+        if time_slice.profiles_1d.f.has_value:
+            assert np.sign(time_slice.profiles_1d.f) == b0sign
+
+        if time_slice.profiles_1d.phi.has_value:
+            assert Approx(time_slice.profiles_1d.phi[0], 0.0, atol=1e-2)
+            assert np.sign(time_slice.profiles_1d.phi[1:]) == b0sign
+
+        if time_slice.global_quantities.q_95.has_value:
+            assert np.sign(time_slice.global_quantities.q_95) == ipsign * b0sign
+
+        if time_slice.profiles_1d.pressure.has_value:
+            assert np.sign(time_slice.profiles_1d.pressure) >= 0.0
+
+        if time_slice.profiles_1d.f.has_value:
+            assert np.sign(time_slice.profiles_1d.f) == b0sign
+
+
 @validator("equilibrium", version="<4.0.0")
 def validate_sign_DD3(ids):
     """
     Validate that the equilibrium IDS has consistent sign conventions.
     """
 
+    # Get the sign of Ip and B0 for each time slice
+    ipsigns, _ = ip_b0_sign(ids)
+
     # Loop through each time slice
     for itime, time_slice in enumerate(ids.time_slice):
 
-        # Get the time for this time slice
-        if ids.ids_properties.homogeneous_time == 0:
-            time = time_slice.time
-        else:
-            time = ids.time[itime]
-
-        # Set sign of Ip
-        if time_slice.global_quantities.ip.has_value:
-            ipsign = np.sign(time_slice.global_quantities.ip)
-        else:
-            ipsign = -9
-
-        # Set sign of B0
-        if ids.vacuum_toroidal_field.b0.has_value:
-            b0 = np.interp(time, ids.time, ids.vacuum_toroidal_field.b0)
-            b0sign = np.sign(b0)
-        else:
-            b0sign = -9
+        # Set the sign of Ip and B0 for this time slice
+        ipsign = ipsigns[itime]
 
         #
         # Validate the sign conventions for EM fields
         #
-
-        if time_slice.profiles_1d.f.has_value:
-            assert np.sign(time_slice.profiles_1d.f) == b0sign
-
-        if time_slice.profiles_1d.phi.has_value:
-            # phi at center found as zero in DINA, CORSICA and etc.
-            assert np.sign(time_slice.profiles_1d.phi[1:]) == b0sign
-
         psi_axis = time_slice.global_quantities.psi_axis
         psi_boundary = time_slice.global_quantities.psi_boundary
-        if psi_axis.has_value and psi_boundary.has_value:
-            assert np.sign(psi_boundary - psi_axis) == ipsign
+        psi_diff = psi_boundary - psi_axis
+        if (
+            psi_axis.has_value
+            and psi_boundary.has_value
+            # Avoid non-physical cases where psi_boundary == psi_axis
+            and np.absolute(psi_diff) > 1e-6
+        ):
+            assert np.sign(psi_diff) == ipsign
+            assert np.sign(time_slice.global_quantities.psi_axis) == -ipsign
 
-        if time_slice.global_quantities.q_95.has_value:
-            assert np.sign(time_slice.global_quantities.q_95) == ipsign * b0sign
+        if time_slice.profiles_1d.dpressure_dpsi.has_value:
+            dpressure_dpsi = time_slice.profiles_1d.dpressure_dpsi
+            assert np.sign(dpressure_dpsi) == -ipsign
 
 
 @validator("equilibrium", version=">=4.0.0")
-def validate_sign(ids):
+def validate_sign_DD4(ids):
     """
     Validate that the equilibrium IDS has consistent sign conventions.
     """
 
+    # Get the sign of Ip and B0 for each time slice
+    ipsigns, _ = ip_b0_sign(ids)
+
     # Loop through each time slice
     for itime, time_slice in enumerate(ids.time_slice):
 
-        # Get the time for this time slice
-        if ids.ids_properties.homogeneous_time == 0:
-            time = time_slice.time
-        else:
-            time = ids.time[itime]
-
-        # Set sign of Ip
-        if time_slice.global_quantities.ip.has_value:
-            ipsign = np.sign(time_slice.global_quantities.ip)
-        else:
-            ipsign = -9
-
-        # Set sign of B0
-        if ids.vacuum_toroidal_field.b0.has_value:
-            b0 = np.interp(time, ids.time, ids.vacuum_toroidal_field.b0)
-            b0sign = np.sign(b0)
-        else:
-            b0sign = -9
+        # Set the sign of Ip and B0 for this time slice
+        ipsign = ipsigns[itime]
 
         #
         # Validate the sign conventions for EM fields
         #
-
-        if time_slice.profiles_1d.f.has_value:
-            assert np.sign(time_slice.profiles_1d.f) == b0sign
-
-        if time_slice.profiles_1d.phi.has_value:
-            # phi at center found as zero in DINA, CORSICA and etc.
-            assert np.sign(time_slice.profiles_1d.phi[1:]) == b0sign
-
         psi_axis = time_slice.global_quantities.psi_axis
         psi_boundary = time_slice.global_quantities.psi_boundary
-        if psi_axis.has_value and psi_boundary.has_value:
-            assert np.sign(psi_boundary - psi_axis) == -ipsign
+        psi_diff = psi_boundary - psi_axis
+        if (
+            psi_axis.has_value
+            and psi_boundary.has_value
+            # Avoid non-physical cases where psi_boundary == psi_axis
+            and np.absolute(psi_diff) > 1e-6
+        ):
+            assert np.sign(psi_diff) == -ipsign
+            assert np.sign(time_slice.global_quantities.psi_axis) == ipsign
 
-        if time_slice.global_quantities.q_95.has_value:
-            assert np.sign(time_slice.global_quantities.q_95) == ipsign * b0sign
+        if time_slice.profiles_1d.dpressure_dpsi.has_value:
+            dpressure_dpsi = time_slice.profiles_1d.dpressure_dpsi
+            assert np.sign(dpressure_dpsi) == ipsign
