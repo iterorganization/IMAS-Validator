0adacdf4055ca98d58ee395faefa31371e116f8a
Sebbe Blokhuizen <sblokhuizen@ignitioncomputing.com>
2026-06-09 16:02:29 +0200

    remove check_imas_version altogether

--- imas_validator/validate/validate.py
+++ imas_validator/validate/validate.py
@@ -35,7 +35,6 @@ def validate(
         List of IDSValidationResult objects
     """
 
-    _check_imas_version()
     try:
         dbentry = imas.DBEntry(imas_uri, "r")
     except (imas.exception.ALException, imas.exception.LowlevelError) as e:
@@ -57,24 +56,3 @@ def validate(
     logger.info(f"{len(results_collection.results)} results obtained")
     dbentry.close()
     return results_collection
-
-
-def _check_imas_version() -> None:
-    """Check if the installed IMAS version is sufficient."""
-    # TODO: check if this is the best level to test for the IMAS version
-    version_found = imas.backends.imas_core.imas_interface.ll_interface._al_version
-    logger.info(f"Found IMAS install with version {version_found}.")
-
-    if imas.backends.imas_core.imas_interface.ll_interface._al_version < Version("5.1"):
-        logger.info(
-            "IDS Validation requires an IMAS installation of version 5.1 or newer."
-            "See the README for more details."
-        )
-
-    # if not imas.backends.imas_core.imas_interface.has_imas:
-    #     raise IMASVersionError()
-    # if imas.backends.imas_core.imas_interface.ll_interface._al_version
-    # < Version("5.1"):
-    #     raise IMASVersionError(
-    #         imas.backends.imas_core.imas_interface.ll_interface._al_version
-    #     )
